cb42d2599134412199f772cb78d53aa508585059
hugo <werwer748@naver.com>
2023-07-19 23:39:44 +0900

    package.json 설정추가(백)

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -6,6 +6,8 @@ const passport = require("passport");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const path = require("path");
+const hpp = require("hpp");
+const helmet = require("helmet");
 
 const userRouter = require("./routes/user");
 const postRouter = require("./routes/post");
@@ -29,10 +31,17 @@ sequelize
 
 passportConfig();
 
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined"));
+  app.use(hpp());
+  app.use(helmet());
+} else {
+  app.use(morgan("dev"));
+}
+
 app.use(
   cors({
-    origin: "http://localhost:3060",
+    origin: ["http://localhost:3060", "nodebird.com"],
     // origin: true,
     // origin: "*", // credentials: true와 같이 사용 못함
     credentials: true, // 쿠키도 같이 보내줌
