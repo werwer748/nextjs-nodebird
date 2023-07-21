1d0649afd76a573c263328425fe87aa13e8f36a9
hugo <werwer748@naver.com>
2023-07-22 00:44:00 +0900

    nginx 설정했고 주소에 https적용

--- prepare/front/config/config.js
+++ prepare/front/config/config.js
@@ -1,1 +1,1 @@
-export const backUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3065' : 'http://api.hugonode.com';
+export const backUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3065' : 'https://api.hugonode.com';
