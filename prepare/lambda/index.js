a630c10cf489b4e2b34fc20992319eb5bd9ad932
hugo <werwer748@naver.com>
2023-07-21 18:00:27 +0900

    코드 변경?

--- prepare/lambda/index.js
+++ prepare/lambda/index.js
@@ -4,33 +4,32 @@ const sharp = require("sharp");
 const s3 = new AWS.S3();
 
 exports.handler = async (event, context, callback) => {
-  const Bucket = event.Records[0].s3.bucket.name; // hugonode.s3
-  const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/123123.jpg
-  console.log("s3 lambda handler ====", Bucket, Key);
+  const Bucket = event.Records[0].s3.bucket.name; // react-nodebird-s3
+  const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/12312312_abc.png
+  console.log(Bucket, Key);
   const filename = Key.split("/")[Key.split("/").length - 1];
   const ext = Key.split(".")[Key.split(".").length - 1].toLowerCase();
   const requiredFormat = ext === "jpg" ? "jpeg" : ext;
-  console.log("filename ====", filename, "ext ====", requiredFormat);
+  console.log("filename", filename, "ext", ext);
 
   try {
     const s3Object = await s3.getObject({ Bucket, Key }).promise();
-    console.log("original ====", s3Object.Body.length);
+    console.log("original", s3Object.Body.length);
     const resizedImage = await sharp(s3Object.Body)
-      .resize(400, 400, {
-        fit: "inside",
-      })
+      .resize(400, 400, { fit: "inside" })
+      .toFormat(requiredFormat)
       .toBuffer();
     await s3
       .putObject({
         Bucket,
         Key: `thumb/${filename}`,
-        body: resizedImage,
+        Body: resizedImage,
       })
       .promise();
-    console.log("put ====", resizedImage.length);
+    console.log("put", resizedImage.length);
     return callback(null, `thumb/${filename}`);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return callback(error);
   }
 };
